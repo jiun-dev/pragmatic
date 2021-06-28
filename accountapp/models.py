19305afb2bc3534eff41535476923e8e8303da6b
jiun-dev <jiun.dev@gmail.com>
2021-06-28 16:39:54 +0900

    migrations

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class HelloWorld(models.Model):
+    # null false -> 무조건 있어야하는 부분
+    text = models.CharField(max_length=255, null=False)
